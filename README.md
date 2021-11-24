d49e45e0f0458bda5b53490e86a036c518f50388
Triple the Boba ! <coconuttt1010@gmail.com>
2021-11-23 23:38:01 -0800

    add asgn

---
+++ README.md
@@ -0,0 +1,4 @@
+## Sketch Practices
+###### Practices that make you have vestibular symtomps 🧠🪓
+---
+1. [Pig-Latin string convert](/w1/pig-latin.js) - convert an input from command line to a simple 'pig latin' code string: pig latin -> igPAY atinLAY
---
+++ w1/pig-latin.js
@@ -0,0 +1,22 @@
+/*
+Write a program that takes any number of command line arguments, converts each to pig latin
+then puts the translated words together into one sentence logged to the console.
+
+Convert a word to pig latin by taking its first letter, moving it to the end of the word, then adding "ay" after it all.
+*/
+
+// take input from command line
+const input = process.argv.slice(2);
+console.log(`----\ninput: ${input.join(' ')}`);
+let translated = [];
+
+// translated func
+const pigLatin = input => {
+  for (let word of input) {
+    translated.push(word.slice(1) + word[0] + 'ay');
+  }
+  return translated;
+};
+
+// display output as a string
+console.log(`-> ${pigLatin(input).join(' ')}\n----`);
